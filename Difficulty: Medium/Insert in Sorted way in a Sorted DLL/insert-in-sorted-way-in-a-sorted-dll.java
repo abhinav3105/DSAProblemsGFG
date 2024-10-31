81ca28448b8756b708634818d62342b483a3de74
Abhinav Dubey <106994648+abhinav3105@users.noreply.github.com>
2024-10-31 16:15:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Insert in Sorted way in a Sorted DLL/insert-in-sorted-way-in-a-sorted-dll.java
@@ -0,0 +1,110 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node {
+    int data;
+    Node prev, next;
+
+    Node(int data) {
+        this.data = data;
+        this.prev = this.next = null;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine(); // Ignore the newline character after t
+
+        while (t-- > 0) {
+            String input = sc.nextLine(); // Read the entire line for the array elements
+            String[] values = input.split(" ");
+
+            Node head = null, tail = null;
+            for (String value : values) {
+                int x = Integer.parseInt(value);
+                if (head == null) {
+                    head = new Node(x);
+                    tail = head;
+                } else {
+                    tail.next = new Node(x);
+                    tail.next.prev = tail;
+                    tail = tail.next;
+                }
+            }
+
+            int valueToInsert = sc.nextInt();
+            if (sc.hasNextLine()) {
+                sc.nextLine(); // Ignore the newline character after the value
+            }
+
+            Solution obj = new Solution();
+            head = obj.sortedInsert(head, valueToInsert);
+            printList(head);
+        
+System.out.println("~");
+}
+
+        sc.close();
+    }
+
+    public static void printList(Node head) {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
+
+// } Driver Code Ends
+
+
+
+
+/*class of the node of the DLL is as
+/*
+class Node {
+    int data;
+    Node prev, next;
+    Node(int data) {
+        this.data = data;
+        this.prev = this.next = null;
+    }
+}
+*/
+class Solution {
+    public Node sortedInsert(Node head, int x) {
+        // add your code here
+        if(x<head.data){
+            Node n = new Node(x);
+            n.next = head;
+            head.prev = n;
+            head = n;
+            return head;
+        }
+        Node temp = head;
+        while(temp!=null){
+            if(temp.data>x){
+                Node n = new Node(x);
+                n.next = temp;
+                n.prev = temp.prev;
+                temp.prev.next = n;
+                temp.prev = n;
+                return head;
+            }
+            temp = temp.next;
+        }
+        Node temp1 = head;
+        while(temp1.next!= null){
+            temp1 = temp1.next;
+        }
+        Node n = new Node(x);
+        temp1.next = n;
+        n.prev = temp1;
+        
+        return head;
+    }
+}
