1183f873f89b2e0819ab575f5f9a127504260380
Abhinav Dubey <106994648+abhinav3105@users.noreply.github.com>
2025-10-06 22:21:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Divisible Numbers/divisible-numbers.java
@@ -0,0 +1,12 @@
+// User function Template for Java
+
+class Solution {
+    static long find(long A, long B) {
+        // code here
+        while(true){
+            A++;
+            if(A%B==0) return A;
+        }
+        //return -1;
+    }
+}
