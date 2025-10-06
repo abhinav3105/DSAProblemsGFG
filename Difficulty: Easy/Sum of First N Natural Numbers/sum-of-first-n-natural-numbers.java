d20e2da6f951eed12ec97a91b013410bf2ef40b8
Abhinav Dubey <106994648+abhinav3105@users.noreply.github.com>
2025-10-06 22:31:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum of First N Natural Numbers/sum-of-first-n-natural-numbers.java
@@ -0,0 +1,13 @@
+// User function Template for Java
+
+class Solution {
+    static int sumOfNaturals(int n) {
+        // code here
+        if(n==0) return n;
+        int ans = 00;
+        for(int i =1;i<=n;i++){
+            ans+=i;
+        }
+        return ans;
+    }
+};
